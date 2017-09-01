6f8da0e1a34b5bbddd6be8b32896ba7af11419b6
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-09-02 05:31:59 +0800

    add get details

--- app/controllers/get_current.rb
+++ app/controllers/get_current.rb
@@ -1,7 +1,29 @@
 require 'json'
+require 'open-uri'
 
 class GetData
 	@jsonData
+	def getResponse(api)
+		if api.length == 1
+			puts api
+			url = api['all']['endpoint']
+			buy = api['all']['buyKey'].split('.')
+			sell = api['all']['sellKey'].split('.')
+			volume = api['all']['volumeKey']
+			if volume 
+				volume = volume.split('.')
+			else
+				volume = -1
+			end
+			begin
+				response = JSON.parse(open(url, read_timeout: 1, open_timeout: 1).read)
+			rescue Exception => e
+				CurrentData::success = false
+			else
+				puts response
+			end
+		end
+	end
 	def getFileContents(file)
 		text = File.read(file)
 		return text
@@ -13,13 +35,11 @@ class GetData
 		for keyCC in @jsonData.keys
 			for keyPC in @jsonData[keyCC].keys
 				for exchange in @jsonData[keyCC][keyPC]
-					puts exchange['api']
+					result = getResponse(exchange['api'])
 				end
 			end
 		end
-
 	end
-
 	def initialize
 		path = './../../exchanges.json'
 		@jsonData = JSON.parse(getFileContents(path))
