3d223177b34540f7f282035666476c628a47e967
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-09-03 19:02:30 +0800

    fetch data and save in dict

--- app/controllers/get_current.rb
+++ app/controllers/get_current.rb
@@ -10,19 +10,28 @@ end
 class GetData
 	@jsonData
 	def getResponse(api)
+		puts "Api"
+		puts api
+		puts "*****************************"
 		currentData = Hash['success'=> false, 'buy'=> -1, 'sell'=> -1, 'volume'=> -1]
+		# Buy, Sell and Volume are at same endpoint
 		if api.length == 1
 			url = api['all']['endpoint']
 			buy = api['all']['buyKey'].split('.')
 			sell = api['all']['sellKey'].split('.')
 			volume = api['all']['volumeKey']
+			# Check if Volume is provided
 			if volume 
 				volume = volume.split('.')
 			else
 				volume = -1
 			end
+			# Get Buy, Sell Price and Volume
 			begin
-				response = JSON.parse(open(url, read_timeout: 1, open_timeout: 1).read)
+				response = JSON.parse(open(url, read_timeout: 1, open_timeout: 5).read)
+				puts "Single"
+				puts response
+				puts '=============================='
 			rescue Exception => e
 				currentData["success"] = false
 				currentData["buy"] = -1
@@ -54,7 +63,81 @@ class GetData
 				end
 				currentData["volume"] = tmp
 				puts currentData
+				puts '=============================='
 			end
+		# Buy, Sell and Volume are at different endpoint
+		else
+			buyEndpoint = api['buy']['endpoint']
+			buyKey = api['buy']['buyKey'].split('.')
+			sellEndpoint = api['sell']['endpoint']
+			sellKey = api['sell']['sellKey'].split('.')
+			if api['volume']
+				volumeEndpoint = api['volume']['endpoint']
+				volumeKey = api['volume']['volumeKey'].split('.')
+			else
+				volumeKey = -1
+			end
+			# Get Buy Price
+			begin
+				response = JSON.parse(open(buyEndpoint, read_timeout: 1, open_timeout: 5).read)
+				puts "Multi Buy"
+				puts response
+				puts '=============================='
+			rescue Exception => e
+				currentData["success"] = false
+				currentData["buy"] = -1
+				currentData["sell"] = -1
+				currentData["volume"] = -1
+				return
+			else
+				tmp = response
+				for key in buyKey
+					key = key.is_i? ? key.to_i : key
+					tmp = tmp[key]
+				end
+				currentData["buy"] = tmp
+			end
+			# Get Sell Price
+			begin
+				response = JSON.parse(open(sellEndpoint, read_timeout: 1, open_timeout: 5).read)
+				puts "Multi Sell"
+				puts response
+				puts '=============================='
+			rescue Exception => e
+				currentData["success"] = false
+				currentData["buy"] = -1
+				currentData["sell"] = -1
+				currentData["volume"] = -1
+				return
+			else
+				currentData["success"] = true
+				tmp = response
+				for key in sellKey
+					key = key.is_i? ? key.to_i : key
+					tmp = tmp[key]
+				end
+				currentData["sell"] = tmp
+			end
+			# Get Volume if provided
+			if volumeKey != -1
+				begin
+					response = JSON.parse(open(volumeEndpoint, read_timeout: 1, open_timeout: 5).read)
+					puts "Multi Volume"
+					puts response
+					puts '=============================='
+				rescue Exception => e
+					currentData["volume"] = -1
+				else
+					tmp = response
+					for key in volumeKey
+						key = key.is_i? ? key.to_i : key
+						tmp = tmp[key]
+					end
+					currentData["volume"] = tmp
+				end
+			end
+			puts currentData
+			puts '=============================='
 		end
 	end
 	def getFileContents(file)
