12df7d708f8673d7ad17b93d9b8a2aca9d891877
imshyam <sschoudhary0001@gmail.com>
2017-09-07 19:15:50 +0800

    after start method

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module RailsAPIs
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.before_initialize do
+    	puts "Yay Initialized"
+    end
   end
 end
