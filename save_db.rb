8c58684b5fc0da232dbeaae37e6918fe95a457fc
imshyam <sschoudhary0001@gmail.com>
2017-09-11 20:09:49 +0800

    add queries

--- save_db.rb
+++ save_db.rb
@@ -1,10 +1,22 @@
 require './get_current.rb'
 require 'sqlite3'
 
+def calculateLastMinMax(curr_data)
+	x = Hash['last_hour_min'=> -1,
+			 'last_day_min'=> -1,
+			 'last_week_min'=> -1,
+			 'last_month_min'=> -1,
+			 'last_hour_max'=> -1,
+			 'last_day_max'=> -1,
+			 'last_week_max'=> -1,
+			 'last_month_max'=> -1]
+	return x
+end
+
 begin
 	db = SQLite3::Database.open "development.sqlite3"
 	db.execute "CREATE TABLE IF NOT EXISTS currents(
-				id INTEGER PRIMARY KEY,
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
         		crypto_curr TEXT,
         		curr TEXT,
         		exchange_id INT,
@@ -21,26 +33,58 @@ begin
         		last_month_max DOUBLE
         		)"
     db.execute "CREATE TABLE IF NOT EXISTS histories(
-				id INTEGER PRIMARY KEY,
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
         		crypto_curr TEXT,
         		curr TEXT,
         		exchange_id INT,
         		date_time DATETIME DEFAULT CURRENT_TIMESTAMP,
         		buy DOUBLE,
-        		sell DOUBLE,
-        		last_hour_min DOUBLE,
-        		last_day_min DOUBLE,
-        		last_week_min DOUBLE,
-        		last_month_min DOUBLE,
-        		last_hour_max DOUBLE,
-        		last_day_max DOUBLE,
-        		last_week_max DOUBLE,
-        		last_month_max DOUBLE
+        		sell DOUBLE
         		)"
     data = GetData.new.getResult
-    query_current = "INSERT OR REPLACE INTO data VALUES("+
-					"SELECT id from currents "
-					+")"
+    for ex_data in data
+    	if ex_data['success']
+	    	last_min_max = calculateLastMinMax(ex_data)
+	   	    query_current = "INSERT OR REPLACE INTO currents VALUES ("+
+							"SELECT id from currents WHERE crypto_curr = \""+
+								ex_data['crypto_curr'] + "\" AND curr = \""+
+								ex_data['curr'] + "\" AND exchange_id = "+
+								ex_data['exchange_id'].to_s + ", " +
+							"\"" + ex_data['crypto_curr'] + "\", " +
+							"\"" + ex_data['curr'] + "\", " +
+							ex_data['exchange_id'].to_s + ", " +
+							Time.now.getutc.to_s + ", " +
+							ex_data['buy'].to_s + ", " + 
+							ex_data['sell'].to_s + ", " + 
+							last_min_max['last_hour_min'].to_s + ", " +
+							last_min_max['last_day_min'].to_s + ", " +
+							last_min_max['last_week_min'].to_s + ", " +
+							last_min_max['last_month_min'].to_s + ", " +
+							last_min_max['last_hour_max'].to_s + ", " +
+							last_min_max['last_day_max'].to_s + ", " +
+							last_min_max['last_week_max'].to_s + ", " +
+							last_min_max['last_month_max'].to_s
+							")"
+			query_history = "INSERT INTO "+
+							"histories (crypto_curr, " +
+									   "curr, " +
+									   "exchange_id, " +
+									   "date_time, " +
+									   "buy, " +
+									   "sell) VALUES (" +
+							"\"" + ex_data['crypto_curr'] + "\", " +
+							"\"" + ex_data['curr'] + "\", " +
+							ex_data['exchange_id'].to_s + ", " +
+							Time.now.getutc.to_s + ", " +
+							ex_data['buy'].to_s + ", " + 
+							ex_data['sell'].to_s +
+							")"
+			puts query_current
+			puts query_history
+		else
+			puts ex_data
+		end
+	end
 rescue SQLite3::Exception => e
 	puts "Exception"
 	puts e
