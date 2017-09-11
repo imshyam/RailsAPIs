0c0c19f73583cee5ad4c09b50d576ee457bff821
imshyam <sschoudhary0001@gmail.com>
2017-09-11 15:58:58 +0800

    remove service

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require_relative 'get_current'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,9 +11,5 @@ module RailsAPIs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
-    config.before_initialize do
-    	puts "Yay Initialized"
-    end
   end
 end
