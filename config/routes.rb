30a04230e8bbbd9e4fbdf546476586f307133e3b
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-09-24 18:10:31 +0800

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :current
+    	get 'get/current'
+    	get 'get/history'
     end
   end
 
-  resources :current
-  root to: 'current#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
