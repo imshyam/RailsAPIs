5bbb5c02611a296aa201e02586e5b2f1f67166a7
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2017-09-24 18:10:22 +0800

    refactor controller and show history

--- app/controllers/api/v1/get_controller.rb
+++ app/controllers/api/v1/get_controller.rb
@@ -1,8 +1,8 @@
 module Api
   module V1
-    class CurrentController < ApplicationController
+    class GetController < ApplicationController
 
-      def index
+      def current
         dataList = Current.all
         @completeData = []
         for data in dataList
@@ -33,9 +33,20 @@ module Api
         end
         render json: @completeData
       end
-    end
-    def history
-        render json: {status: "Hello"}, status: :ok
+      def history
+        dataList = History.all
+        @completeData = []
+        for data in dataList
+          dataJson = Hash["crypto_curr" => data.crypto_curr,
+                    "curr" => data.curr,
+                    "exchange_id" => data.exchange_id,
+                    "date_time" => data.date_time,
+                    "buy" => data.buy,
+                    "sell" => data.sell]
+          @completeData.push(dataJson)
+          puts dataJson
+        end
+        render json: @completeData
       end
     end
   end
